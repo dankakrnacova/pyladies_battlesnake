ddec97946fc0b6cf40419451ba38d6651d4e2cec
dankakrnacova <78499388+dankakrnacova@users.noreply.github.com>
2021-10-28 18:53:49 +0000

    add logic to avoid wall

--- server.py
+++ server.py
@@ -25,9 +25,9 @@ def handle_info():
     return {
         "apiversion": "1",
         "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
+        "color": "#FFDB58",  # TODO: Personalize
+        "head": "smile",  # TODO: Personalize
+        "tail": "pixel",  # TODO: Personalize
     }
 
 
--- server_logic.py
+++ server_logic.py
@@ -61,10 +61,22 @@ def choose_move(data: dict) -> str:
     possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
 
     # TODO: Using information from 'data', find the edges of the board and don't let your Battlesnake move beyond them
-    # board_height = ?
-    # board_width = ?
 
+    board_height = data["board"]["height"]
+    board_width = data["board"]["width"]
+
+    if my_head["x"] == 0:
+        possible_moves.remove("left")
+    if my_head["y"] == 0:
+        possible_moves.remove("down")
+    if my_head["x"] == board_width - 1:
+        possible_moves.remove("right")
+    if my_head["y"] == board_height - 1:
+        possible_moves.remove("up")
+   
     # TODO Using information from 'data', don't let your Battlesnake pick a move that would hit its own body
+
+    
 
     # TODO: Using information from 'data', don't let your Battlesnake pick a move that would collide with another Battlesnake
 
